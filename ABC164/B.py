11d1b48e8ddcbf1343672ce4e976d040ec156a97
Mao Kurihara <mao.kurihara@gmail.com>
2023-02-26 10:33:05 +0900

    2023_0225

---
+++ ABC164/B.py
@@ -0,0 +1,16 @@
+# B - Battle
+t_hp, t_power, a_hp, a_power = list(map(int,input().split()))
+flg = True
+
+while flg:
+    a_hp = a_hp - t_power
+    if a_hp <= 0:
+        flg = False
+        break
+    t_hp = t_hp - a_power
+    if t_hp <= 0:
+        flg = False
+        break
+
+print("Yes" if t_hp > a_hp else "No")
+
