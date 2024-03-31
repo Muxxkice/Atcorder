ba45550b46905adfd82182f2242f727f3cd2d62b
Mao Kurihara <mao.kurihara@gmail.com>
2024-03-31 23:39:34 +0900

    2023_03_31

---
+++ src/ABC/ABC312/a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(void){
+   string s;
+   cin >> s;
+   cout << (s == "ACE" || s == "BDF" || s == "CEG" || s == "DFA" || s == "EGB" || s == "FAC" || s == "GBD" ? "Yes" : "No") << endl;
+
+}
